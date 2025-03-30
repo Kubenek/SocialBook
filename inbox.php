f49b91edce967ed4567a4a27c7026dad09824543
Kubenek <Kubenku.of@gmail.com>
2025-03-30 17:08:24 +0200

    initial add for inbox files

---
+++ inbox.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/icons/favicon.ico" type="image/x-icon">
+    <title>Inbox | SocialBook</title>
+
+    <link href="styles/scrollbar.css" rel="stylesheet"> 
+    <link href="styles/basics.css" rel="stylesheet">
+
+
+</head>
+<body>
+    
+</body>
+</html>
