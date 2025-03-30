4e01f9551ca1f77b306db8ccfcb4be5c2407fd20
Kubenek <Kubenku.of@gmail.com>
2025-03-30 16:33:46 +0200

    update sql file with new structure

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,6 +1,8 @@
 document.querySelectorAll('.dropdown-button').forEach(function(button) {
     button.addEventListener('click', function() {
+
         var dropdownContent = button.nextElementSibling;
+
         dropdownContent.classList.toggle('show');
         button.classList.toggle('active')
     });
