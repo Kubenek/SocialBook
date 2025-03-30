11af0e1125f7704d855094594aec4d17c64831c8
Kubenek <Kubenku.of@gmail.com>
2025-03-30 16:21:32 +0200

    minor fixes

--- scripts/char_counter.js
+++ scripts/char_counter.js
@@ -4,4 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
         let currentLength = this.value.length;
         document.getElementById("charCount").textContent = `${currentLength}/${maxChars}`;
     });
+
+    var counter = document.getElementById("charCount");
+    var maxChars = 120;
+    var bioLength = document.getElementById("bioInput").value.length;
+    counter.textContent = `${bioLength}/${maxChars}`
 })
