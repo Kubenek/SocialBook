aae7d1935434b08ef56244ba8918b2fe191dd8ca
Kubenek <Kubenku.of@gmail.com>
2025-04-03 06:28:58 +0200

    add img

--- UI/cr-modal/modal.php
+++ UI/cr-modal/modal.php
@@ -1,23 +1,17 @@
 <div class="modal" aria-labelledby="modalTitle" aria-hidden="true">
     <div class="modal-content">
         
-        <!-- Modal Close Button -->
         <span class="close-btn" id="closeModalBtn">&times;</span>
 
-        <!-- Modal Title -->
         <h1 id="modalTitle">Create a Post</h1>
 
-        <!-- Form -->
         <form method="POST" action="scripts/php/submit-post.php">
-            <!-- Title Input -->
             <label for="title">Title</label>
             <input type="text" name="title" id="title" required>
 
-            <!-- Content Input -->
             <label for="content">Post Content</label>
             <textarea id="content" name="content" required style="resize: none;"></textarea>
 
-            <!-- Submit Button -->
             <button type="submit" name="post-create">Post!</button>
         </form>
 
