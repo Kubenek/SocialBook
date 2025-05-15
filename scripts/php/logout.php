75e6a6d1008d6be1e09877276eeb2d22a65b74e0
Kubenek <Kubenku.of@gmail.com>
2025-05-15 09:23:29 +0200

    add saving theme to database on logout

--- scripts/php/logout.php
+++ scripts/php/logout.php
@@ -7,6 +7,16 @@ if(session_status() == PHP_SESSION_NONE) {
 $ssid = session_id();
 $conn = new mysqli("localhost", "root", "", "dane");
 
+$Tsql = "UPDATE `users` SET `theme` = ? WHERE `id` = ?";
+$cur_id = include("fetch_id.php");
+$currTheme = $_SESSION['dark_status'];
+
+$stmt = $conn->prepare($Tsql);
+$stmt->bind_param("ii", $currTheme, $cur_id);
+$stmt->execute();
+$stmt->close();
+
+
 $sql = "DELETE FROM `session` WHERE `session_id` = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("s", $ssid);
