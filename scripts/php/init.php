1c3962c58833192c0db5422bcba0accdad3a69aa
Kubenek <Kubenku.of@gmail.com>
2025-05-17 17:21:06 +0200

    add init file

---
+++ scripts/php/init.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+use Dotenv\Dotenv;
+
+if(!isset($_ENV['DB_HOST'])) {
+    $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+    $dotenv->load();
+}
+
+require_once __DIR__ . '/fetch_database_connection.php';
+
+$conn = loadConn();
